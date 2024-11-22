ced868f2b9e1781c7f2effca828fa62c50c234e0
izra4 <aryaizra2@gmail.com>
2024-11-22 20:13:31 +0700

    feat(facility): add facility trpc router to appRouter

--- src/server/trpc/root.ts
+++ src/server/trpc/root.ts
@@ -2,11 +2,13 @@ import { router } from "./router";
 import { helloRouter } from "./routers/hello";
 import { userRouter } from "./routers/user";
 import { genreRouter } from "@/server/trpc/routers/genre";
+import { facilityRouter } from "@/server/trpc/routers/facility";
 
 export const appRouter = router({
   hello: helloRouter,
   user: userRouter,
   genre: genreRouter,
+  facility: facilityRouter,
 });
 
 export type AppRouter = typeof appRouter;
