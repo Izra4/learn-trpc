53635040d9ac8e06a3ccfc2aa537c17d419c75f2
izra4 <aryaizra2@gmail.com>
2024-11-22 10:37:34 +0700

    feat(trpc): add genreRouter to appRouter

--- src/server/trpc/root.ts
+++ src/server/trpc/root.ts
@@ -1,10 +1,12 @@
 import { router } from "./router";
 import { helloRouter } from "./routers/hello";
 import { userRouter } from "./routers/user";
+import { genreRouter } from "@/server/trpc/routers/genre";
 
 export const appRouter = router({
   hello: helloRouter,
   user: userRouter,
+  genre: genreRouter,
 });
 
 export type AppRouter = typeof appRouter;
