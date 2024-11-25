08ba17c7e89c71533fdbffbc7a63e326b6750f1c
izra4 <aryaizra2@gmail.com>
2024-11-25 11:34:59 +0700

    feat(studio): add dummy tRPC for studio

--- src/server/trpc/root.ts
+++ src/server/trpc/root.ts
@@ -3,12 +3,14 @@ import { helloRouter } from "./routers/hello";
 import { userRouter } from "./routers/user";
 import { genreRouter } from "@/server/trpc/routers/genre";
 import { facilityRouter } from "@/server/trpc/routers/facility";
+import { studioRouter } from "@/server/trpc/routers/studio";
 
 export const appRouter = router({
   hello: helloRouter,
   user: userRouter,
   genre: genreRouter,
   facility: facilityRouter,
+  studio: studioRouter,
 });
 
 export type AppRouter = typeof appRouter;
